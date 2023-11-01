d9b8a2cdceb1679edc2d3abff33fa7b92af3ab5f
WangZhijun <zhijun.wang@maxjia.com>
2023-11-01 10:46:32 +0800

    init

--- app/.cxx/MiaoMiaoDebug/401f114x/arm64-v8a/Assimp/revision.h
+++ app/.cxx/MiaoMiaoDebug/401f114x/arm64-v8a/Assimp/revision.h
@@ -1,7 +1,7 @@
 #ifndef ASSIMP_REVISION_H_INC
 #define ASSIMP_REVISION_H_INC
 
-#define GitVersion 0x0
-#define GitBranch "HEAD"
+#define GitVersion 0xdcb4428
+#define GitBranch "main"
 
 #endif // ASSIMP_REVISION_H_INC
